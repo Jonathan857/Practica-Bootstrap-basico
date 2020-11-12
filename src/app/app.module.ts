3d383502f6a5d2d292ef8db81b09fe19e55149bc
JIMENEZ HERNANDEZ <jonathan.jimenez-hernandez@capgemini.com>
2020-11-11 21:33:23 -0600

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+//Rutas
+import { APP_ROUTING } from './app.routes'; 
+
+//Servicios
+import {HeroesService} from './servicios/heroes.service';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -16,9 +23,13 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
